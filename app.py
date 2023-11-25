214dcb0d1b604c929803de0fcb0897aa288e9a50
SawBan <108050827+ReXiOP@users.noreply.github.com>
2023-11-25 20:15:35 +0600

    Update app.py

--- app.py
+++ app.py
@@ -50,5 +50,5 @@ def result():
     except Exception as e:
         return jsonify({'error': str(e)})
 
-if __name__ == '__main__':
-    app.run(debug=True,port=5001)
+# if __name__ == '__main__':
+#     app.run(debug=True,port=5001)
