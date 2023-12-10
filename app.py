3003d08cd2d5adf53792e0a622f859e92e246bcd
SawBan <rexisajid5@gmail.com>
2023-12-10 14:32:00 +0600

    new

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ import numpy as np
 from PIL import Image
 from io import BytesIO
 import torch
+import ssl
+import certifi
+from datetime import datetime
+import json
 
 app = Flask(__name__)
 
@@ -33,9 +37,19 @@ def detect_human(image):
 
 def detect_animal(image):
     # Class indices for cat and dog
-    animal_class_indices = [14,15, 16, 17, 18, 19, 20, 21, 22, 77]
-
+    animal_class_indices = [14, 15, 16, 17, 18, 19, 20, 21, 22, 77]
     return detect_objects(image, animal_class_indices)
+
+def save_to_log(url, result):
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    log_entry = {"timestamp": timestamp, "url": url, "result": result}
+
+    try:
+        with open("log.json", "a") as log_file:
+            json.dump(log_entry, log_file)
+            log_file.write('\n')
+    except Exception as e:
+        print(f"Error saving to log: {e}")
 
 @app.route('/')
 def index():
@@ -47,8 +61,8 @@ def result():
         # Get the image URL from the request
         image_url = request.form['image_url']
 
-        # Download the image
-        response = urllib.request.urlopen(image_url)
+        # Download the image using requests
+        response = urllib.request.urlopen(image_url, context=ssl.create_default_context(cafile=certifi.where()))
         image = response.read()
 
         # Detect humans and animals
@@ -57,12 +71,14 @@ def result():
 
         # Return result in JSON format
         result = {'face_detected': face_detected, 'animal_detected': animal_detected}
+
+        # Save the result to the log
+        save_to_log(image_url, result)
+
         return jsonify(result)
 
     except Exception as e:
         return jsonify({'error': str(e)})
 
-
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
-
